bb374a669157ad0539c5a43ec1d2f922c4633c9c
Jan Heczko <jan.heczko@gmail.com>
2014-11-28 15:37:19 +0100

    Fixed the remaining occurences of Struct-like syntaxe. Tested on actual .tra files.

--- load_tra.py
+++ load_tra.py
@@ -18,9 +18,9 @@ def load_single_tra(tra_name, colsep=';', cols=[], skip_lines=2):
                 columns = [float(col.strip()) for col in columns]
                 data.append(columns)
             lnum += 1
-    out = {'name':tra_name, 'data':np.array(data).T[cols,:])
+    print(tra_name, ' data.shape =', np.array(data).shape)
+    out = {'name':tra_name, 'data':np.array(data).T[cols,:]}
     return out
-
 
 def load_tras(tra_names, colsep=';', cols=[], skip_lines=2):
     """
@@ -31,67 +31,21 @@ def load_tras(tra_names, colsep=';', cols=[], skip_lines=2):
         out.append(load_single_tra(tra_name, colsep=colsep, cols=cols, skip_lines=skip_lines))
     return out
 
-
-def split_cycles(data, t_row=3, d_row=1, up_treshold=.1, down_treshold=-.1, start_up=True):
-    """
-    Splits measured data into loading/unloading cycles.
-    Returns list of data-like arrays.
-    """
-    # diff data - left differences are used.
-    ddata = np.diff(data[d_row]) / np.diff(data[t_row])
-
-    ups = ddata < down_treshold
-    dns = ddata > up_treshold
-    current_up = start_up
-    start_pt = 0
-    out = []
-    for i in range(len(ddata)):
-        if (current_up and dns[i]) or (not(current_up) and ups[i]):
-            stop_pt = i+1
-            out.append(np.array([d[start_pt:stop_pt] for d in data]))
-            current_up = not(current_up)
-            start_pt = stop_pt
-    return out
-
-def smooth_data(data, cols=[0,], n=1, w=None):
-    """
-    Smoothes given columns in data and reshapes the other to the same dimension.
-    """
-    if w is None:
-        w = np.ones(2 * n + 1)
-    print 'w:', w
-    out = []
-    for ii, cc in enumerate(data):
-        if ii in cols:
-            out.append(np.array([
-                sum(w * cc[jj - n : jj + n + 1]) / sum(w) for jj in range(n, len(cc) - n)]))
-        else:
-            out.append(np.array(cc[n:-n]))
-    return out
-
 if __name__ == '__main__':
     tra_files = sys.argv[1:]
-    data = load_tras(tra_files, cols=[0,1,2,3], skip_lines=2)
+    data = load_tras(tra_files, colsep=' ', cols=[0,1,2], skip_lines=2)
     for dat in data:
-        print dat.name, ' -> ', dat.name[dat.name.rfind('/') + 1:dat.name.rfind('.')]
-        dat['name'] = dat['name'][dat['name'].rfind('/') + 1:dat['name'].rfind('.')]))
+        dat['name'] = dat['name'][dat['name'].rfind('/') + 1:dat['name'].rfind('.')]
 
-    cols = ('0', '.3', '.5')
-    mark_styles = ('o', '^', 'v', 's', '>', '<')
-    fs = 20
-
-    # plot force vs. time
+    # plot force vs. displacement
     plt.figure()
-    leg = []
     for i, dat in enumerate(data):
-        plt.plot(dat.data[3,:] / 3600.,dat.data[0,:] - dat.data[0,0],
-                 marker = mark_styles[i % len(mark_styles)], markevery=(i * len(dat.data[3,:]) / 20, len(dat.data[3,:]) / 6), markersize=8, #markerfacecolor = '.9',
-                 label = dat.name)
-    plt.xlabel('time [h]', fontsize=fs)
-    plt.ylabel('force [N]', fontsize=fs)
-    plt.legend(loc=0, fontsize=fs)
-    plt.setp(plt.gca().get_xticklabels() + plt.gca().get_yticklabels(), fontsize=fs)
+        plt.plot(dat['data'][1,:], dat['data'][0,:],
+                 label = dat['name'])
+    plt.xlabel('displacement [mm]')
+    plt.ylabel('force [N]')
+    plt.legend(loc=0)
+    plt.ylim((0, 40))
     plt.grid()
-    plt.tight_layout()
-
+    
     plt.show()
