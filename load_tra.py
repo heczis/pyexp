227b5953013968a5f2439da97625d4e2e5204d0a
Jan Heczko <jan.heczko@gmail.com>
2016-12-21 15:04:15 +0100

    Clean-up and various [almost forgotten] changes

--- load_tra.py
+++ load_tra.py
@@ -4,41 +4,43 @@ columns, each row corresponds to one time instant).
 """
 import numpy as np
 import pylab as plt
-import sys
-import io
 import os
 import argparse
 
-def load_single_tra(tra_name, colsep=';', cols=[], skip_lines=2):
+def load_single_tra(tra_name, colsep=';', cols=None, skip_lines=2):
     """
-    Loads a single .tra file named tra_name, skipping skip_lines first lines, columns separated by colsep, rows by newline.
-    Returns columns with indices given in cols (in given order) as a list; nonexistent indices raise error.
+    Loads a single .tra file named tra_name, skipping skip_lines first
+    lines, columns separated by colsep, rows by newline.
+    Returns columns with indices given in cols (in given order) as a
+    list; nonexistent indices raise error.
     """
-    data = list()
-    with io.open(tra_name, encoding='latin-1') as f:
-        for lnum, line in enumerate(f):
-            if lnum >= skip_lines:
-                columns = line.strip().split(colsep)
-                columns = [
-                    float(col.strip().replace(',','.'))
-                    for col in columns if len(col)>0]
-                data.append(columns)
-    print(tra_name, ' data.shape =', np.array(data).shape)
-    out = {'name':tra_name, 'data':np.array(data).T[cols,:]}
+    data = []
+    if cols is not None:
+        with open(tra_name, 'r') as f:
+            for lnum, line in enumerate(f):
+                if lnum >= skip_lines:
+                    columns = line.strip().split(colsep)
+                    columns = [
+                        float(col.strip().replace(',', '.'))
+                        for col in columns if len(col) > 0]
+                    data.append(columns)
+            print(tra_name, ' data.shape =', np.array(data).shape)
+    out = {'name':tra_name, 'data':np.array(data).T[cols, :]}
     return out
 
-def load_tras(tra_names, colsep=';', cols=[], skip_lines=2):
+def load_tras(tra_names, colsep=';', cols=None, skip_lines=2):
     """
     Loads given .tra files into a list of load_single_tra's outputs.
     """
-    out = list()
-    for tra_name in tra_names:
-        out.append(load_single_tra(tra_name, colsep=colsep, cols=cols, skip_lines=skip_lines))
+    out = []
+    if cols is not None:
+        for tra_name in tra_names:
+            out.append(load_single_tra(tra_name, colsep=colsep, cols=cols, skip_lines=skip_lines))
     return out
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description = 'Load given TRA files and plot the data.')
+        description='Load given TRA files and plot the data.')
     parser.add_argument(
         'file', metavar='FILE', nargs='+', type=str,
         help='TRA file(s) to be read.')
@@ -52,14 +54,14 @@ if __name__ == '__main__':
         '--skiplines', metavar='SKIP', type=int, default='2',
         help='how many lines to skip at the beginning of each TRA file')
     args = parser.parse_args()
-    
+
     tra_files = args.file
-    colsep = args.colsep
+    column_separator = args.colsep
     colnum = args.colnum
     skiplines = args.skiplines
 
     if tra_files:
-        data = load_tras(tra_files, colsep=colsep,
+        data = load_tras(tra_files, colsep=column_separator,
                          cols=range(colnum), skip_lines=skiplines)
         for dat in data:
             dat['name'] = dat['name'][dat['name'].rfind(os.sep) + 1:dat['name'].rfind('.')]
@@ -67,11 +69,11 @@ if __name__ == '__main__':
         # plot force vs. displacement
         plt.figure()
         for i, dat in enumerate(data):
-            plt.plot(dat['data'][1,:], dat['data'][0,:],
-                     label = dat['name'])
+            plt.plot(dat['data'][1, :], dat['data'][0, :],
+                     label=dat['name'])
         plt.xlabel('displacement [mm]')
         plt.ylabel('force [N]')
         plt.legend(loc=0)
         plt.grid()
-        
+
         plt.show()
--- process_data.py
+++ process_data.py
@@ -3,34 +3,51 @@ Provides functions for operating on experimental data.
 """
 import numpy as np
 
-def split_cycles(data, t_row=0, d_row=1, up_treshold=.1, down_treshold=-.1, start_up=True):
+def diff(row, dnum=2):
+    """
+    Auxliary function for implementing finite differences.
+    """
+    return row[dnum:] - row[:-dnum]
+
+def get_strain_rate(data, td_row=(0, 1), dnum=1):
+    # diff data - left differences are used.
+    ddata = diff(data[td_row[1]], dnum) / diff(data[td_row[0]], dnum)
+    return ddata
+
+def split_cycles(data, td_row=(0, 1), tresholds=(.1, -.1), start_up=True, dnum=1):
     """
     Splits measured data into loading/unloading cycles according to
     strain rate.
 
     data : np.array, each rows correspond to measured quantities
-    t_row : which row of data contains measured time
-    d_row : which row of data contains measured displacement
+    td_row : which row of data contains measured time and displacement
 
-    up-/down_treshold : strain rates at which loading/unloading is
-      is considered as started
+    tresholds : strain rates at which loading/unloading is
+      is considered as started. Has two components, first is the up-loading
+      rate, second is the down-loading rate.
+
+    start_up : boolean
+      If True, the first loading cycle should start in tension.
+
+    dnum : int
+      Finite-differences step (in array indices)
 
     Returns list of data-like arrays.
     """
     # diff data - left differences are used.
-    ddata = np.diff(data[d_row]) / np.diff(data[t_row])
+    ddata = diff(data[td_row[1]], dnum) / diff(data[td_row[0]], dnum)
 
-    ups = ddata < down_treshold
-    dns = ddata > up_treshold
+    ups = ddata > tresholds[0]
+    dns = ddata < tresholds[1]
     current_up = start_up
     start_pt = 0
     out = []
     for i in range(len(ddata)):
-        if ((current_up and dns[i]) or (not(current_up) and ups[i])
-            or i == len(ddata)-1):
+        if (current_up and dns[i]) or (not(current_up) and ups[i]) \
+           or (i == len(ddata)-1):
             stop_pt = i+1
             out.append(np.array([d[start_pt:stop_pt] for d in data]))
-            current_up = not(current_up)
+            current_up = not current_up
             start_pt = stop_pt
     return out
 
